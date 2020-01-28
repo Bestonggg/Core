cfdc3043d77d7a72422dd486c464781a097b5fc7
429284-21 <429284-21>
2020-01-28 17:27:29 +0300

    4.1

---
+++ LogInterface.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    interface LogInterface
+    {
+        //void log(string s);
+        //     void write();
+    }
+}
